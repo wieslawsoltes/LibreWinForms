fb41035e2b4ba17e7ed626a26232a70d2688dff1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-29 08:40:44 +0200

    Prepare LibreWinForms 0.1.0-preview.32

--- eng/librewinforms-pack.sh
+++ eng/librewinforms-pack.sh
@@ -11,7 +11,7 @@ export DOTNET_ROLL_FORWARD="${DOTNET_ROLL_FORWARD:-Major}"
 export DOTNET_ROLL_FORWARD_TO_PRERELEASE="${DOTNET_ROLL_FORWARD_TO_PRERELEASE:-1}"
 
 package_output="${LIBREWINFORMS_PACKAGE_OUTPUT:-${repo_root}/artifacts/packages/Release/NonShipping}"
-dev_package_version="${LIBREWINFORMS_DEV_PACKAGE_VERSION:-0.1.0-preview.31}"
+dev_package_version="${LIBREWINFORMS_DEV_PACKAGE_VERSION:-0.1.0-preview.32}"
 bridge_package_version="${LIBREWINFORMS_BRIDGE_PACKAGE_VERSION:-${dev_package_version}}"
 progpu_package_version="${LIBREWINFORMS_PROGPU_PACKAGE_VERSION:-0.1.0-preview.28}"
 configuration="${LIBREWINFORMS_CONFIGURATION:-Release}"
